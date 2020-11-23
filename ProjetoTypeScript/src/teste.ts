9708f915063a4d03903b22842a40ded034569a25
BrunoResende89 <brunofast89@gmail.com>
2020-11-23 13:37:24 -0300

    alteração hoje

--- ProjetoTypeScript/src/teste.ts
+++ ProjetoTypeScript/src/teste.ts
@@ -1,5 +1,6 @@
-import Person from './Person.js'
-import Book from './Book.js'
-import Periodical from './Periodical.js'
+import {Person} from './Person.js'
+import {Book} from './Book.js'
+import {Periodical} from './Periodical.js'
+import {Gender} from './Gender.js'
 
-
+let person1 = new Person("bruno", new Date(1989, 12,02), Gender.Male)
