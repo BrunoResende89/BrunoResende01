ce3e50913ea95f07730cbe8a3e07ce42d516b41e
BrunoResende89 <brunofast89@gmail.com>
2020-11-27 17:19:04 -0300

    atualização

--- ProjetoTypeScript/public/entities/Gender.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Gender = void 0;
-var Gender;
-(function (Gender) {
-    Gender["Female"] = "f";
-    Gender["Male"] = "m";
-})(Gender = exports.Gender || (exports.Gender = {}));
-exports.default = Gender;
